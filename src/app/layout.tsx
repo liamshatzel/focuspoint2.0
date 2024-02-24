ba0b764cbc65152ac7029900d868ccccb5908840
liamshatzel <liam.shatzel@gmail.com>
2024-02-23 23:50:57 -0800

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,12 +12,28 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
+
+//export default function RootLayout({
+//  children,
+//}: Readonly<{
+//  children: React.ReactNode;
+//}>) {
+//  return (
+//    <html lang="en">
+//      <body className={inter.className}>{children}</body>
+//    </html>
+//  );
+//}
+
+//#DA371E
